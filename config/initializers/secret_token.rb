135544f5e83940f5cb97bc5f2e184705135dd15d
Sean Slattery <sean.slattery203@gmail.com>
2013-02-09 20:58:21 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CharityBrawl::Application.config.secret_token = 'c1504240367b072493fbbbac46675ffb7f1a819aaf0ce0cec592b9877da7313029c2adbb167e6abb1cc1f00ffb61cd48d5cdaf896107f442036e76a3a7d88f0f'
